7e53ff83daafe8494804f286f6f7b32d78296baf
vinaxi.khalasi <kvinaxi@gmail.com>
2022-11-23 14:13:15 +0530

    [server] initial set-up done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css';
 
 const App = () =>{
+  const [ listening, setListening ] = useState(false);
+
+  useEffect( () => {
+    if (!listening) {
+      const events = new EventSource('http://localhost:3001/events');
+
+      events.onmessage = (event) => {
+        const parsedData = JSON.parse(event.data);
+        console.log({parsedData});
+
+      };
+      setListening(true);
+    }
+  }, [listening]);
+
   return (
     <div className="App">
       <header className="App-header">
--- server/server_v2.js
+++ server/server_v2.js
@@ -2,15 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+var callFactApiOnInterval = require('./helpers/callApi')
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/status', (request, response) => response.json({clients: clients.length}));
 
-const PORT = 3000;
+const PORT = 3001;
 
 let clients = [];
 let facts = [];
@@ -19,8 +20,6 @@ app.listen(PORT, () => {
   console.log(`Facts Events service listening at http://localhost:${PORT}`)
 })
 
-// ...
-
 function eventsHandler(request, response, next) {
     const headers = {
       'Content-Type': 'text/event-stream',
@@ -28,39 +27,52 @@ function eventsHandler(request, response, next) {
       'Cache-Control': 'no-cache'
     };
     response.writeHead(200, headers);
-  
+
     const data = `data: ${JSON.stringify(facts)}\n\n`;
+  console.log({data})
+    //response.write(data);
+    // const clientId = Date.now();
   
-    response.write(data);
+    // const newClient = {
+    //   id: clientId,
+    //   response
+    // };
   
-    const clientId = Date.now();
+    // clients.push(newClient);
   
-    const newClient = {
-      id: clientId,
-      response
-    };
-  
-    clients.push(newClient);
-  
-    request.on('close', () => {
-      console.log(`${clientId} Connection closed`);
-      clients = clients.filter(client => client.id !== clientId);
-    });
+    // request.on('close', () => {
+    //   console.log(`${clientId} Connection closed`);
+    //   clients = clients.filter(client => client.id !== clientId);
+    // });
   }
   
   app.get('/events', eventsHandler);
 
-  // ...
 
 function sendEventsToAll(newFact) {
     clients.forEach(client => client.response.write(`data: ${JSON.stringify(newFact)}\n\n`))
   }
   
-  async function addFact(request, respsonse, next) {
-    const newFact = request.body;
-    facts.push(newFact);
-    respsonse.json(newFact)
-    return sendEventsToAll(newFact);
+  async function addFact(request, respsonse) {
+  //  console.log({request, respsonse});
+   // const newFact = request.json()
+    try{
+
+    console.log({request});
+
+   // facts.push(newFact);
+    
+    //respsonse.json(newFact)
+
+   // console.log({newFact});
+}
+    catch(error){
+        console.log({error});
+    }
+  //  return sendEventsToAll(newFact);
   }
   
-  app.post('/fact', addFact);
+ app.post('/fact', addFact);
+
+ console.log('hello there!');
+ callFactApiOnInterval()
