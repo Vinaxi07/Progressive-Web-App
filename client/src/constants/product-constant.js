932b2c659f33d21fcfbe416797f6caf0d4ea4cc8
ankita.a.nandi <ankita.nandi3094@gmail.com>
2022-11-23 18:18:11 +0530

    [feat] Basic grif design implemented

---
+++ client/src/constants/product-constant.js
@@ -0,0 +1,43 @@
+
+export default [
+    {
+        id:1,
+        name:'iphone',
+        image:require("../assets/image/iphone.png"),
+        offer:'60% off',
+        claimed:"10%",
+        price:'60'
+    },
+    {
+        id:2,
+        name:'headphones',
+        image:require('../assets/image/headphone.jpeg'),
+        offer:'60% off',
+        claimed:"10%",
+        price:'60'
+    },
+    {
+        id:3,
+        name:'smart-watch',
+        image:require('../assets/image/smart-watch.png'),
+        offer:'60% off',
+        claimed:"10%",
+        price:'60'
+    },
+    {
+        id:4,
+        name:'sim-card',
+        image:require('../assets/image/sim-card.png'),
+        offer:'60% off',
+        claimed:"10%",
+        price:'60'
+    },
+    {
+        id:5,
+        name:'macbook',
+        image:require('../assets/image/macBook.png'),
+        offer:'60% off',
+        claimed:"10%",
+        price:'60'
+    },
+]
