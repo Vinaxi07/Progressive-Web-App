c80caac0256e841ffab8b16243b6f8219706e4ac
vinaxi.khalasi <kvinaxi@gmail.com>
2022-11-24 13:38:20 +0530

    [server]

--- server/helpers/getProductsUpdates.js
+++ server/helpers/getProductsUpdates.js
@@ -1,11 +1,40 @@
 const { getRandomEleFromArray, getRndInteger } = require('./utility')
 
-module.exports = function (products) {
-  let updatedProduct = getRandomEleFromArray(products);
+module.exports = {
 
-  if (updatedProduct && "price" in updatedProduct) {
-    updatedProduct.price = getRndInteger(50, 70);
+  productsList: class {
+    constructor(products) {
+      this.products = products||[]
+    }
+
+    updateProductList(updatedProduct) {
+
+      let updatedProductIndex = this.products.findIndex(
+        (product) => product.id === updatedProduct.id
+      );
+
+      if (updatedProductIndex !== -1) {
+        let updatedProducts = [...this.products];
+        updatedProducts[updatedProductIndex] = {
+          ...this.products[updatedProductIndex],
+          ...updatedProduct,
+        };
+        this.products  = updatedProducts
+      }
+    }
+  },
+
+  updateProductDetails: function (products) {
+    let updatedProduct = getRandomEleFromArray(products);
+
+    if (updatedProduct && "price" in updatedProduct) {
+      updatedProduct.price = getRndInteger(50, 70);
+    }
+
+    if (updatedProduct && "claimed" in updatedProduct) {
+      updatedProduct.claimed = updatedProduct.claimed + 1;
+    }
 
     return updatedProduct
   }
-};
+}
