ab3f54cc52c1a06705ca43cc60d0e14b753f386a
ankita.a.nandi <ankita.nandi3094@gmail.com>
2022-11-25 17:33:51 +0530

    linear progress bar is added

--- client/src/components/product-list/ProductsList.tsx
+++ client/src/components/product-list/ProductsList.tsx
@@ -1,11 +1,12 @@
 import React, { useCallback, useEffect, useState } from "react";
 import "./product-list.css";
 import { ProductsList as ProductsListConst } from "../../constants/index.js";
+import {LinearProgress,Typography} from '@mui/material';
 
 const serverBaseURL = "http://localhost:3000";
 
 const ProductsList = () => {
-  let [productsList, setProductsList] = useState(ProductsListConst);
+  const [productsList, setProductsList] = useState(ProductsListConst);
 
   useEffect(() => {
     // connect sse endpoint
@@ -58,10 +59,44 @@ const ProductsList = () => {
         setProductsList(updatedProducts);
       }
       //  console.log({parsedUpdates})
-    },
-    [productsList]
+    },[productsList]
   );
 
+  const ProdFunction = ()=> {
+   return( productsList.map((item,index) => {
+    console.log(item.claimed)
+      return (
+        <div className="grid-item" key={index}>
+          <div className="grid-image">
+            <img className="img-src" src={`${item.image}`} />
+          </div>
+          <div className="grid-name padding-top-10">{item.name}</div>
+          <div className="grid-offer padding-top-10">{item.offer}</div>
+          <div className="grid-price padding-top-10">{item.price}</div>
+          <div className="grid-progressbar padding-top-10">
+
+          {/* <Box sx={{ display: 'flex', alignItems: 'center' ,width: '100%' }}>
+              <Box sx={{ width: '100%', mr: 1 }}>
+                <LinearProgress variant="determinate" value={item.claimed} />
+              </Box>
+              <Box sx={{ minWidth: 35 }}>
+                <Typography variant="body2" color="text.secondary">{`${item.claimed}%`}</Typography>
+              </Box>
+            </Box> */}
+            
+            <div className="grid-progressBar">
+              <div className="progressBar-loader">
+               <LinearProgress variant="determinate" value={item.claimed} color="error"/>
+              </div>
+              <div className="progressBar-percentage">
+             {`Claimed ${item.claimed}%`}
+               </div>
+            </div>
+          </div>
+        </div>
+      );
+    }))
+  }
   return (
     <div className="wrapper">
       <header>
@@ -69,19 +104,7 @@ const ProductsList = () => {
       </header>
 
       <div className="grid-container">
-        {productsList.map((item,index) => {
-          return (
-            <div className="grid-item" key={index}>
-              <div className="grid-image">
-                <img className="img-src" src={`${item.image}`} />
-              </div>
-              <div className="grid-name">{item.name}</div>
-              <div className="grid-offer">{item.offer}</div>
-              <div className="grid-price">{item.price}</div>
-              <div className="grid-progressbar">{item.claimed}</div>
-            </div>
-          );
-        })}
+        {ProdFunction()}
       </div>
     </div>
   );
